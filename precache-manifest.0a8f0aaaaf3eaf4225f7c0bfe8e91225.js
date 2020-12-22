2e7b30e1c5a484dd9ddb8c564bee492b86f1b05e
YOUR_USERNAMErroy11705 <rroy11705@gmail.com>
2020-12-22 16:53:36 +0530

    Updates

---
+++ precache-manifest.0a8f0aaaaf3eaf4225f7c0bfe8e91225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3ca6b79e27dcbe0c2f05745dee2bb0",
+    "url": "/shadesGenerator/index.html"
+  },
+  {
+    "revision": "538cf8f74d72e47ed16f",
+    "url": "/shadesGenerator/static/css/main.9d95e5c9.chunk.css"
+  },
+  {
+    "revision": "5d365b6c09267480acd3",
+    "url": "/shadesGenerator/static/js/2.8a645749.chunk.js"
+  },
+  {
+    "revision": "c5908504bef8b63ceceaa7902193523a",
+    "url": "/shadesGenerator/static/js/2.8a645749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538cf8f74d72e47ed16f",
+    "url": "/shadesGenerator/static/js/main.1859d238.chunk.js"
+  },
+  {
+    "revision": "54a78c1f704229fe448a",
+    "url": "/shadesGenerator/static/js/runtime-main.959c8546.js"
+  }
+]);
